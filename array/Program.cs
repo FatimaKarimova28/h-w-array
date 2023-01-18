6e849b5bcfd2540f37a3b9bb00a032f402191f37
FatimaKarimova28 <fatimaika@code.edu.az>
2023-01-18 15:47:50 +0400

    array done

---
+++ array/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+// Console.WriteLine("Hello, World!");
+
+
+// int[] array = new int[] { 100, 300, 200, 50 };
+//for (int i = 0; i < array.Length; i++)
+//{
+//	int minIndex = i;
+//	for (int j = i; j < array.Length; j++)
+//	{
+//		if (array[minIndex] > array[j])
+//		{
+//			minIndex = j;
+//		}
+//	}
+
+//	int k = array[i];
+//	array[i] = array[minIndex];
+//	array[minIndex] = k;
+
+//	Console.WriteLine(array[i]);
+
+
+
+
+//int sum = 0;
+//double avarage = 0;	
+//int[] array = new int[] { 1, 2, 3, 4, 5 };
+//for (int i = 0; i < array.Length; i++)
+
+//{
+//	sum += array[i];
+//}
+
+//avarage = sum / array.Length;
+//Console.WriteLine(avarage);
+
+
+
+
+
+//int[] array = new int[] { 1, 2, 3, 4, 5 };
+
+//for (int i = array.Length; i >= 0; i --)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+
+
+
+
+
+
+//	{
+
+//	}
+//}
+
